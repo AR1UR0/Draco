e6b266b47f76e9db0ac7984a5a9cc847c15f7efc
Arturo <arturo@abastos.org>
2026-01-26 08:33:47 +0100

    index terminado (sin enlaces, imágenes ni offcanvas)

---
+++ Draco/js/index/carrousel.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const track = document.getElementById("carouselTrack");
+    let isMoving = false;
+
+    function getItemWidth() {
+        const item = track.children[0];
+        const style = getComputedStyle(item);
+        const margin =
+            parseFloat(style.marginLeft) + parseFloat(style.marginRight);
+
+        return item.offsetWidth + margin;
+    }
+
+    function moveCarousel() {
+        if (isMoving) return;
+        isMoving = true;
+
+        const itemWidth = getItemWidth();
+
+        track.style.transition = "transform 0.5s ease";
+        track.style.transform = `translateX(-${itemWidth}px)`;
+
+        setTimeout(() => {
+            track.style.transition = "none";
+            track.appendChild(track.firstElementChild);
+            track.style.transform = "translateX(0)";
+            isMoving = false;
+        }, 500);
+    }
+
+    setInterval(moveCarousel, 2000);
+});
